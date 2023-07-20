389923475a6aa72ca7d91eab509a5f121bf005e2
ZhannaOsypenko <josikadp@gmail.com>
2023-07-20 10:13:37 +0200

    add js task

--- script/script.js
+++ script/script.js
@@ -1,65 +1,87 @@
 console.log("Hello world!!!");
 
-function count(string) {
-    console.log("string => ",string);
-    let strObj = {};
-    if (string.length !== 0) {
-      for (const key of string){
-        // console.log("i = ",i, "  ", !strObj.hasOwnProperty(string[i]));
-        if (!strObj.hasOwnProperty(key)){
-          strObj[key] = 0;
-        }
-        strObj[key]++;
-      }
-      return strObj;
+function eleven(n) {
+  // let strN = n.toString();
+  let arrN = String(n).split('').map(Number);;
+
+  console.log("arrN => ", arrN);
+  let even = arrN.reduce((sum,elem,ind) => {
+    // console.log(sum,elem,ind);
+    if (ind%2 == 0) {
+      sum += elem;
     }
-    return {};
+    return sum;
+  },0);
+
+  let odd = arrN.reduce((sum,elem,ind) => {
+    if (ind%2 != 0) {
+      sum += elem;
+    }
+    return sum;
+  },0);
+
+  console.log("even = ", even);
+  console.log("odd = ", odd);
+
+  return ((even-odd)%11 === 0)?
+  `YES, the number ${n} is divisible by 11`:
+  `NO, the number ${n} is NOT divisible by 11`;
+
 }
 
-let str1 = "aaabccdabb";
-console.log(count(str1));
+console.log(eleven(625581));
 
-console.log("============");
-// let str2 = "aaabccdadb";
-// console.log(str2.replace(/a/g, ''));
-// console.log(str2.split('a'));
+console.log("===================");
+// let a=1342;
+// console.log(a.toString().split(""));
 
-function howMuchILoveYou(nbPetals) {
-    let arr = ["I love you",
-    "a little",
-    "a lot",
-    "passionately",
-    "madly",
-    "not at all"];
+// let arr2 = ["Hello","World"];
+// let arr3 = ["Ok!","Zhanna"];
 
-    let index = ((nbPetals % (arr.length)) === 0 ) ? 
-    5 : (nbPetals % arr.length) - 1;
+// console.log("arr = ", arr2);
+// function transposeTwoStrings (array) {
+//   let word1 = array[0];
+//   let word2 = array[1];
+//   let a = array.map((elem) => elem.length);
+//   let maxLengthWord = Math.max(...a);
+//   let result = "";
+//   for (let i = 0; i < maxLengthWord; i++) {
+//     let x = word1[i]?word1.substr(i, 1):"_";
+//     let y = word2[i]?word2.substr(i, 1):"_";
+    
+//     result += (i !== (maxLengthWord - 1))?
+//     `${x} ${y}\n`:`${x} ${y}`;
+//   }
+//   return result;
+// }
 
-  
-    return nbPetals <= arr.length ? arr[nbPetals-1]: arr[index]
+// console.log(transposeTwoStrings(arr3));
+
+function isPalindrom (str) {
+  return (str === str.split("").reverse().join(""))?
+  true:false;
 }
 
-console.log(howMuchILoveYou(169));
 
-console.log("============");
 
-let arrA = [3,2,1,7,4,1,5,7,1];
-// let arrB = arrA.join().replaceAll(1,0).split(",");
-// console.log("arrB => ",arrB);
-for (let i = 0; i < arrA.length; i++) {
-  if (arrA[i] === 1) {
-    arrA = arrA.with(i, 'ok');
-  } 
+let a = "123321";
+let b = "123210";
+let c = "";
+let d = "45654";
+let e = "1";
+
+
+// console.log(b.split("").reverse().join(""));
+
+console.log(isPalindrom(e));
+
+// =======================
+
+function creditCard (cardNumber) {
+  return (cardNumber.substr(0,4) + " **** **** " + cardNumber.substr(-4,4))
 }
-console.log(arrA);
 
-// let a= 5;
-// a = 1;
-// console.log(a);
-let str = "zhanna_osypenko";
-console.log(str.replaceAll("n","N"));
-console.log(arrA.join(""));
-console.log(arrA.join().split(","));
-
-
-
+let card1 = "1234432156788765";
+let card2 = "7777432156782222";
+// console.log(card1.substr(0,4) + " **** **** " + card1.substr(-4,4));
+console.log(creditCard(card2));
